bf21df8e3a6a6df1d5720b6c40c1dbd5dc739e36
ratel <ratel_chen@163.com>
2015-05-14 20:13:16 +0800

    no comments

--- userspace/daemon/vsecplat_record.c
+++ userspace/daemon/vsecplat_record.c
@@ -1,15 +1,76 @@
+#include "nm_jhash.h"
 #include "vsecplat_record.h"
 
-static struct record_bucket *record_bucket_hash;
+static struct record_bucket *record_bucket_hash=NULL;
+
+#define VSECPLAT_RECORD_HASH_SIZE (1<<10)
 
 int vsecplat_init_record_bucket(void)
 {
-	
+	int idx=0;
+	struct record_bucket *bucket=NULL;
+	record_bucket_hash = (struct record_bucket *)malloc(VSECPLAT_RECORD_HASH_SIZE * sizeof(struct record_bucket));
+
+	if(NULL==record_bucket_hash){
+		// TODO
+		return -1;
+	}
+	memset(record_bucket_hash, 0, VSECPLAT_RECORD_HASH_SIZE * sizeof(struct record_bucket));
+	for(idx=0;idx<VSECPLAT_RECORD_HASH_SIZE;idx++){
+		bucket = record_bucket_hash + idx;
+		INIT_LIST_HEAD(&bucket->list);
+		nm_mutex_init(&bucket->mutex);
+	}
+	return 0;
+}
+
+static u32 record_hash(struct record_entry *tmp)
+{
+	return (jhash_3words(tmp->sip, tmp->dip, tmp->sport|(tmp->dport<<16), 0) % VSECPLAT_RECORD_HASH_SIZE);
+}
+
+static int test_record_match(const struct record_entry *entry, const struct record_entry *tmp)
+{
 	return 0;
 }
 
 int vsecplat_record_pkt(struct nm_skb *skb)
 {
+	u32 hash=0;
+	struct record_bucket *bucket=NULL;
+	struct record_entry *entry=NULL;
+	struct record_entry *tmp=NULL;	
+
+	tmp = (struct record_entry *)malloc(sizeof(struct record_entry));
+	if(NULL==tmp){
+		//TODO
+		return -1;
+	}
+	memset(&tmp, 0, sizeof(struct record_entry));
+	INIT_LIST_HEAD(&tmp->list);
+	tmp->sip = skb->nh.iph->saddr;
+	tmp->dip = skb->nh.iph->daddr;
+	tmp->proto = skb->nh.iph->protocol;
+	if(tmp->proto==6){
+		tmp->sport = skb->h.uh->source;
+		tmp->dport = skb->h.uh->dest;
+	}else if(tmp->proto==17){
+		tmp->sport = skb->h.th->source;
+		tmp->dport = skb->h.th->dest;
+	}
+	tmp->vlanid = skb->vlanid;
+
+	hash = record_hash(tmp);
+	bucket = record_bucket_hash + hash;
+	nm_mutex_lock(&bucket->mutex);
+	entry = LIST_FIND(&record_bucket_hash->list, test_record_match, struct record_entry *, tmp);
+	if(NULL!=entry){
+		entry->count++;
+	}else{
+		tmp->count++;
+		list_add_tail(&bucket->list, &tmp->list);
+	}
+	nm_mutex_unlock(&bucket->mutex);
 	return 0;
 }
 
--- userspace/daemon/vsecplat_record.h
+++ userspace/daemon/vsecplat_record.h
@@ -18,10 +18,21 @@ struct record_entry{
 
 struct record_bucket{
 	struct list_head list;
+	u32 count;
 	struct nm_mutex mutex;
 };
 
 int vsecplat_init_record_bucket(void);
 int vsecplat_record_pkt(struct nm_skb *skb);
 
+#define LIST_FIND(head, cmpfn, type, args...) 	\
+		({ 										\
+		 const struct list_head *__i, *__j=NULL;\
+		 list_for_each(__i, (head))				\
+		 if(cmpfn((const type)__i, ##args)){	\
+			 __j = __i;							\
+			 break;								\
+		 }										\
+		 (type)__j;								\
+		 })
 #endif
